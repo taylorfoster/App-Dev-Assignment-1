4e1b6967755ae35419ccb9ca16157db5c0b6f9ba
Taylor Foster <taylor.foster@d25s70nngq18m.oc.edu>
2016-09-01 17:10:18 -0500

    First commit for homework 1 projects

---
+++ Chap 6 Bronze.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var number: Int = 0
+for var j = 0; j < 5; j++ {
+    for var i = 0; i < 100; i++ {
+        number += 2
+
+    }
+}
